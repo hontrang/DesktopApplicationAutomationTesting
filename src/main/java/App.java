ade19a54b61fdc1b079189c87324887de35267b8
hon.trang <hon.trang@isobar.com>
2024-06-24 15:57:11 +0700

    fix: add log

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,9 +7,11 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.io.File;
 import java.net.URL;
+import java.util.logging.Logger;
 
 public class App {
     private static final boolean RUNNING_FOREVER = true;
+    private static Logger logger = Logger.getLogger(App.class.getName());
     private static String source;
 
     public static void main(String[] args) {
@@ -26,7 +28,7 @@ public class App {
             while (RUNNING_FOREVER) {
                 source = driver.getPageSource();
                 fileHelper.writeFile(System.getProperty("user.dir") + File.separator + "app.xml", source);
-                System.out.println(XMLHelper.getText(System.getProperty("user.dir") + File.separator + "app.xml", "//android.widget.TextSwitcher"));
+                logger.info(XMLHelper.getText(System.getProperty("user.dir") + File.separator + "app.xml", "//android.widget.TextSwitcher"));
                 sleep(1000);
             }
         } catch (Exception e) {
